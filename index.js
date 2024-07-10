dbec9c5f44a6b1713172278bb24555185dc45e62
FRANK7434 <sonfransiscojames015@gmail.com>
2024-07-10 23:34:45 +0300

    add array filter

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Code your solution here
-// Code your solution here
 function findMatching(drivers, name) {
     return drivers.filter(
       (driver) => driver.toLowerCase() === name.toLowerCase()
